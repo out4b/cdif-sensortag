880ddc5273ffc35aab79a58f09ad9e6d998af396
out4b <seeds.c@gmail.com>
2015-10-06 17:31:09 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+/*
+	CDIF SensorTag module implementation
+	This module is based on Sandeep Mistry's SensorTag library for node.js to
+	control TI BLE based SensorTag.
+*/
+
 module.exports = require('./lib/sensortag');
